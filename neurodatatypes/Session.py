f363e7cf51cab222e7161a53583430a2f8b61537
Max Melin <mmelin@g.ucla.edu>
2022-12-16 12:15:35 -0800

    skeleton for where preprocessing utils will go

--- neurodatatypes/Session.py
+++ neurodatatypes/Session.py
@@ -8,43 +8,62 @@ from itertools import repeat
 from datetime import datetime
 import numpy as np
 
-#TODO: Create Subject object and a file to save metadata to
 class Session():
     def __init__(self, datapath, animal, date):
 
         self.datapath = datapath
         self.animal = animal
-        self.date = date #TODO: Make this a datetime?
+        self.date = date
         self.data = None #only generate pandas dataframe if required.
         self.bhvpath = paths.get_behavior_path(datapath, animal, date)
-        self.metadata = load_session_metadata(self.bhvpath) #TODO: maybe pin to a metadata file for quicker access
+        self.metadata = load_session_metadata(self.bhvpath)
         self.data, self.stimtimes = load_session_dataframe(self.bhvpath)
     
-    def get_trial_indices(self): #returns trial numbers based on desired condition
+    def get_trial_indices(self): 
+        """
+        returns trial numbers based on desired condition
+        """
         raise NotImplementedError()
         
-    def get_expected_states(self,GLMHMM): #TODO: probably move to glmhmm object
-        raise NotImplementedError() 
-        
+    def get_event_times(self, alignment_event):
+        raise NotImplementedError()
+        # This will return a dataframe of times relative to a specific trial event
+        # dubbed alignment_event. 
+        # The dataframe will probably need two levels of arrays. 
+            
     @staticmethod
-    def return_dates(animal):
-        '''
-        This function will return session dates for an animal that satisfy
-        a certain criteria. create this function after done with other methods 
-        that return performance and metadata for a session.
-        '''
-        raise NotImplementedError()        
-        
-    @staticmethod
-    def get_sessions(dpath, animal, max_nochoice = None, modality = None, min_trials = None, singlespout_cutoff = None, assisted_cutoff = None, discrim_min = None, discrim_max = None, min_percent_correct = None): #TODO: Make general purpose for dataset, or move it
-    #TODO: get by date
-        '''
-        Loads all availible sessions into list in parallel
-        '''
+    def get_sessions(dpath, #TODO: Make general purpose for dataset, or move it
+                     animal,
+                     max_nochoice = None,
+                     modality = None,
+                     min_trials = None,
+                     singlespout_cutoff = None,
+                     assisted_cutoff = None,
+                     discrim_min = None,
+                     discrim_max = None,
+                     min_percent_correct = None): 
+        """Returns a list of Session instances that satisfy the filtered criteria that is given to the function.
+
+        Args:
+            dpath (_type_): _description_
+            ormoveitanimalmax_nochoice (_type_, optional): _description_. Defaults to None.
+            modality (_type_, optional): _description_. Defaults to None.
+            min_trials (_type_, optional): _description_. Defaults to None.
+            singlespout_cutoff (_type_, optional): _description_. Defaults to None.
+            assisted_cutoff (_type_, optional): _description_. Defaults to None.
+            discrim_min (_type_, optional): _description_. Defaults to None.
+            discrim_max (_type_, optional): _description_. Defaults to None.
+            min_percent_correct (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+
         searchdir = os.path.join(dpath,animal,'SpatialDisc')
         dates = [date for date in os.listdir(searchdir) if os.path.isdir(os.path.join(searchdir, date))] #get folders only
         
         #sessions = [Session(dpath,animal,date) for date in dates] #single process
+
         with Pool() as pool: #multiple processes
             sessions = pool.starmap(Session, zip(repeat(dpath), repeat(animal), dates))
         
--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 from neurodatatypes import *
 from sklearn.decomposition import PCA
 import numpy as np
